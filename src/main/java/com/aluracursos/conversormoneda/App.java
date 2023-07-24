842ce740cc070fbfe87c6af7ba63520ce37f1f64
Afranio Solano <argus@Mac-mini-de-Afranio.local>
2023-07-24 18:57:34 -0500

    feat: core

---
+++ src/main/java/com/aluracursos/conversormoneda/App.java
@@ -0,0 +1,13 @@
+package com.aluracursos.conversormoneda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
